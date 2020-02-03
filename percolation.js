286946326a8ce918bd3c5bd4df57372a5dbcbcb6
Dmitry Vlaznev <dmitry.vlaznev@datadvance.net>
2020-02-03 21:04:14 +0300

    A percolation problem is completed.

--- percolation.js
+++ percolation.js
@@ -9,7 +9,7 @@
 
 // https://coursera.cs.princeton.edu/algs4/assignments/percolation/specification.php
 
-/*jshint esversion: 6 */
+const { performance } = require("perf_hooks");
 
 // *********************************************************************
 // UnionFindCompressed
@@ -20,7 +20,10 @@ class UnionFindCompressed {
      * @param {number} n
      */
     constructor(n) {
-        this.parents = Array.apply(null, new Array(n)).map((v, i) => i);
+        this.parents = new Array(n);
+        for (let i = 0; i < n; i++) {
+            this.parents[i] = i;
+        }
     }
 
     /**
@@ -69,10 +72,25 @@ class Percolation {
      * @param {number} n
      */
     constructor(n) {
-        this.field = Array.apply(null, new Array(n)).map(row => {
-            Array.apply(null, new Array(n)).map(() => -1);
-        });
+        this.n = n;
+        this.field = Array.apply(null, new Array(n)).map(row =>
+            Array.apply(null, new Array(n)).map(() => 0),
+        );
+
         this.openSitesCounter = 0;
+        this.uf = new UnionFindCompressed(n * n + 2);
+        this.upVirtualRootIndex = this.n * this.n;
+        this.downVirtualRootIndex = this.upVirtualRootIndex + 1;
+    }
+
+    /**
+     * Convert 2D field index to 1D union-find index.
+     *
+     * @param {number} row
+     * @param {number} col
+     */
+    _toFlatIndex(row, col) {
+        return row * this.n + col;
     }
 
     /**
@@ -81,7 +99,37 @@ class Percolation {
      * @param {number} row
      * @param {number} col
      */
-    open(row, col) {}
+    open(row, col) {
+        if (this.isOpen(row, col)) {
+            return;
+        }
+        this.field[row][col] = 1;
+        this.openSitesCounter++;
+        const currentIndex = this._toFlatIndex(row, col);
+
+        // Check virtual nodes connectivity.
+        if (row === 0) {
+            this.uf.union(currentIndex, this.upVirtualRootIndex);
+        } else if (row === this.n - 1) {
+            this.uf.union(currentIndex, this.downVirtualRootIndex);
+        }
+
+        // Check vertical connectivity.
+        if (row > 0 && this.isOpen(row - 1, col)) {
+            this.uf.union(currentIndex, this._toFlatIndex(row - 1, col));
+        }
+        if (row < this.n - 1 && this.isOpen(row + 1, col)) {
+            this.uf.union(currentIndex, this._toFlatIndex(row + 1, col));
+        }
+
+        // Check horizontal connectivity.
+        if (col > 0 && this.isOpen(row, col - 1)) {
+            this.uf.union(currentIndex, this._toFlatIndex(row, col - 1));
+        }
+        if (col < this.n - 1 && this.isOpen(row, col + 1)) {
+            this.uf.union(currentIndex, this._toFlatIndex(row, col + 1));
+        }
+    }
 
     /**
      * Is the site (row, col) open?
@@ -91,7 +139,9 @@ class Percolation {
      *
      * @returns {boolean} Check result.
      */
-    isOpen(row, col) {}
+    isOpen(row, col) {
+        return !!this.field[row][col];
+    }
 
     /**
      * Is the site (row, col) full?
@@ -101,21 +151,27 @@ class Percolation {
      *
      * @returns {boolean} Check result.
      */
-    isFull(row, col) {}
+    isFull(row, col) {
+        return !this.field[row][col];
+    }
 
     /**
      * Returns the number of open sites.
      *
      * @returns {boolean} Tne requested number.
      */
-    numberOfOpenSites() {}
+    numberOfOpenSites() {
+        return this.openSitesCounter;
+    }
 
     /**
      * Does the system percolate?
      *
      * @returns {boolean} Tne requested info.
      */
-    percolates() {}
+    percolates() {
+        return this.uf.connected(this.upVirtualRootIndex, this.downVirtualRootIndex);
+    }
 }
 
 // *********************************************************************
@@ -128,67 +184,121 @@ class PercolationStats {
      * @param {number} n
      * @param {number} trials
      */
-    constructor(n, trials) {}
+    constructor(n, trials) {
+        this.n = n;
+        this.trials = trials;
+
+        this.results = null;
+        this.meanValue = null;
+        this.deviation = null;
+        this.low = null;
+        this.hi = null;
+    }
+
+    /**
+     * Do a Monte-Carlo simulation and print results.
+     */
+    calculate() {
+        this.results = new Array(this.trials);
+        this.meanValue = null;
+        this.deviation = null;
+        this.low = null;
+        this.hi = null;
+
+        console.log("------------------------------------------------");
+        console.log(`Start ${this.trials} experiments for ${this.n}*${this.n} filed`);
+        console.log("------------------------------------------------");
+
+        const start = performance.now();
+        for (let i = 0; i < this.trials; i++) {
+            this.results[i] = this.percolate();
+        }
+        const stop = performance.now();
+        const nSquared = this.n ** 2;
+        const time = stop - start;
+        console.log(`Execution time is      ${(time / 1000).toPrecision(4)} seconds`);
+        console.log(`Spent                  ${(time / nSquared).toPrecision(4)} ms on an item`);
+        console.log(`Mean value is          ${this.mean() / nSquared}`);
+        console.log(`Standard deviation is  ${this.stdDev() / nSquared}`);
+        console.log("------------------------------------------------");
+    }
+
+    /**
+     * Generate a random cell. Every cell will be touched once only.
+     */
+    *getRandomCell() {
+        const l = this.n * this.n;
+        let i, row, col;
+        const indices = new Array(l);
+        for (let i = 0; i < l; i++) {
+            indices[i] = i;
+        }
+        // Randomize an array using Fisher–Yates shuffle
+        // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+        for (i = l - 1; i > 0; i--) {
+            const r = Math.floor(Math.random() * Math.floor(i - 1));
+            [indices[r], indices[i]] = [indices[i], indices[r]];
+        }
+        i = 0;
+        while (i < l) {
+            row = Math.floor(indices[i] / this.n);
+            col = indices[i] % this.n;
+            yield { row, col };
+            i++;
+        }
+    }
+
+    /**
+     * Do one simulation.
+     */
+    percolate() {
+        const infeasibleThreshold = this.n * (this.n - 1) + 2;
+        const gen = this.getRandomCell();
+        const p = new Percolation(this.n);
+        while (!p.percolates()) {
+            const cell = gen.next();
+            if (cell.done) {
+                throw new Error("All cells are opened!");
+            }
+            if (p.numberOfOpenSites() >= infeasibleThreshold) {
+                throw new Error("Must percolate!");
+            }
+            p.open(cell.value.row, cell.value.col);
+        }
+        return p.numberOfOpenSites();
+    }
 
     /**
      * Sample mean of percolation threshold.
      *
      * @returns {number}
      */
-    mean() {}
+    mean() {
+        if (!this.meanValue) {
+            this.meanValue =
+                this.results.reduce((acc, v) => (acc = acc + v), 0) / this.trials;
+        }
+
+        return this.meanValue;
+    }
 
     /**
      * Sample standard deviation of percolation threshold.
      *
      * @returns {number}
      */
-    stdDev() {}
-
-    /**
-     * Low endpoint of 95% confidence interval.
-     *
-     * @returns {number}
-     */
-    confidenceLo() {}
-
-    /**
-     * High endpoint of 95% confidence interval.
-     *
-     * @returns {number}
-     */
-    confidenceHi() {}
+    stdDev() {
+        if (!this.deviation) {
+            const mean = this.mean();
+            this.deviation = Math.sqrt(
+                this.results.reduce((acc, v) => (acc = acc + (v - mean) ** 2), 0) /
+                    (this.trials - 1),
+            );
+        }
+        return this.deviation;
+    }
 }
 
-const u = new UnionFindCompressed(16);
-u.union(0, 1);
-u.union(1, 2);
-u.union(1, 5);
-u.union(6, 5);
-u.union(6, 10);
-
-u.union(3, 7);
-
-u.union(4, 9);
-u.union(12, 9);
-u.union(14, 9);
-u.union(14, 15);
-
-console.log("--------");
-console.log("connected = ", u.connected(0, 6));
-console.log("connected = ", u.connected(5, 2));
-console.log("connected = ", u.connected(9, 14));
-console.log("connected = ", u.connected(12, 15));
-
-console.log("--------");
-console.log("disconnected = ", u.connected(0, 11));
-console.log("disconnected = ", u.connected(8, 13));
-console.log("disconnected = ", u.connected(12, 10));
-
-console.log("--------");
-console.log(
-    Array.apply(null, new Array(16))
-        .map((v, i) => (i < 10 ? "0" + i : "" + i))
-        .join(" "),
-);
-
-console.log(u.parents.map(v => (v < 10 ? "0" + v : "" + v)).join(" "));
+new PercolationStats(10, 100).calculate();
+new PercolationStats(100, 100).calculate();
+new PercolationStats(1000, 100).calculate();
